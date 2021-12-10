531d1387af38d80706609807906f978d1d0d1cd3
Ngocnguyen2212 <ntngoc2212@gmail.com>
2021-12-10 20:42:50 +0700

    bo sung

--- src/ss02_loop/bai_tap/HienThi20SoNguyenToDauTien.java
+++ src/ss02_loop/bai_tap/HienThi20SoNguyenToDauTien.java
@@ -8,11 +8,25 @@ public class HienThi20SoNguyenToDauTien {
         Scanner input = new Scanner(System.in);
         System.out.print("How many number you want to print?: ");
         numbers = input.nextInt();
-        int count = 0;
-        int N = 2;
-        for (;count< numbers;) {
-            if (N%) {
+        int count = 1;
+        int N = 3;
+        int i = 2;
+        System.out.println("Số nguyên tố đầu tiên là: 2");
+
+        while (count< numbers) {
+            boolean check = true;
+            while (i < N) {
+                if (N % i == 0) {
+                    check = false;
+                    break;
+                }
+                i++;
             }
+            if (check) {
+                System.out.println("Số nguyên tố thứ " + (count+1) + " là: " + N);
+            }
+            count ++;
+            N++;
         }
     }
 }
